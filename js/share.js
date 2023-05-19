7de6692016316f8cf2f7c11e524e97f100f4ae9f
HJSHIN92 <tls0901kr@gmail.com>
2023-05-19 10:46:48 +0900

    delete / share and result 0

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-const url = 'https://mbtitestfirsttry.netlify.app/';
+const url = 'https://mbtitestfirsttry.netlify.app';
 
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
