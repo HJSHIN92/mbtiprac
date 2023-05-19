dbd4b88fd224477b496fd75e53796d1900d72290
HJSHIN92 <tls0901kr@gmail.com>
2023-05-19 10:55:44 +0900

    .. > .

--- js/share.js
+++ js/share.js
@@ -1,11 +1,11 @@
-const url = 'https://mbtitestfirsttry.netlify.app';
+const url = 'https://mbtitestfirsttry.netlify.app/';
 
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = 'J 테스트 결과는 바로바로 .. ';
   const shareDes = infoList[resultAlt].name;
-  const shareImage = url + '../img/image-' + resultAlt + '.png';
+  const shareImage = url + './img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
 
   Kakao.init('ef1b16b02d2eb1256c7e156b614c7572');
